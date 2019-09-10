78a2c08adfcdd24d3976b91e521cc2ddde8ef3cc
Souleymane <souleymane11dembele@gmail.com>
2019-09-10 00:56:19 -0700

    Site map

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,8 +40,92 @@ export default {
   ** Nuxt.js modules
   */
   modules: [
-
+    ['@nuxtjs/sitemap', {
+      gzip: true,
+      exclude: [
+        '/secret',
+        '/admin'
+      ],
+    }]
   ],
+  // sitemap: {
+  //       exclude: [
+  //     '/secret',
+  //     '/admin/**'
+  //   ],
+  // },
+  sitemap: {
+    filter ({ routes, options }) {
+      if (options.hostname === 'https://sdembele.me/') {
+        return routes.filter(route => route.locale === 'en')
+      }
+      return routes.filter(route => route.locale === 'fr')
+    }
+  },
+  sitemap: {
+    filter ({ routes }) {
+      return routes.map(route => {
+        route.url = `${route.url}/`
+        return route
+      })
+    }
+  },
+  sitemap: {
+    xmlNs: 'xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"',
+  },
+  sitemap: {
+    defaults: {
+      changefreq: 'daily',
+      priority: 1,
+      lastmod: new Date(),
+      lastmodrealtime: true
+    }
+  },
+  // sitemap: {
+  //   hostname: 'http://localhost:3000/',
+  //   gzip: true,
+  //   exclude: [
+  //     '/secret',
+  //     '/admin/**'
+  //   ],
+  //   routes: [
+  //     '/',
+  //     {
+  //       url: '/',
+  //       changefreq: 'daily',
+  //       priority: 1,
+  //       lastmodISO: '2019-09-30T13:30:00.000Z'
+  //     },
+  //     '/about/',
+  //     {
+  //       url: '/about/',
+  //       changefreq: 'daily',
+  //       priority: 2,
+  //       lastmodISO: '2019-09-30T13:30:00.000Z'
+  //     },
+  //     '/projects',
+  //     {
+  //       url: '/projects/',
+  //       changefreq: 'daily',
+  //       priority: 3,
+  //       lastmodISO: '2019-09-30T13:30:00.000Z'
+  //     },
+  //     '/lab',
+  //     {
+  //       url: '/lab/',
+  //       changefreq: 'daily',
+  //       priority: 4,
+  //       lastmodISO: '2019-09-30T13:30:00.000Z'
+  //     },
+  //     '/contact/',
+  //     {
+  //       url: '/contact/',
+  //       changefreq: 'daily',
+  //       priority: 5,
+  //       lastmodISO: '2019-09-30T13:30:00.000Z'
+  //     }
+  //   ]
+  // },
   /*
   ** Build configuration
   */
